9b33e3b0195331cd5f3407474175397c5105a483
purpleplum <ya@manhangroup.com>
2024-10-10 23:17:37 -0700

    create postgis in public

--- rails/db/migrate/20221022183839_add_postgis_extension_to_database.rb
+++ rails/db/migrate/20221022183839_add_postgis_extension_to_database.rb
@@ -1,10 +1,5 @@
 class AddPostgisExtensionToDatabase < ActiveRecord::Migration[7.0]
   def change
-    # create a heroku_ext schema
-    execute <<-SQL
-      CREATE schema public;
-    SQL
-    
     execute <<-SQL
       CREATE EXTENSION IF NOT EXISTS postgis
       SCHEMA public;
